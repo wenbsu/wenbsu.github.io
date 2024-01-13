b140804939664a0605498ed2fc50ca10b16f9a70
weyoxu <weyoxu@gmail.com>
2024-01-13 23:24:03 +0800

    修改代码

--- sw.js
+++ sw.js
@@ -20,7 +20,6 @@ const PRECACHE_LIST = [
   "./js/hux-blog.min.js",
   "./js/snackbar.js",
   "./img/icon_wechat.png",
-  "./img/avatar-hux.jpg",
   "./img/home-bg.jpg",
   "./img/404-bg.jpg",
   "./css/hux-blog.min.css",
